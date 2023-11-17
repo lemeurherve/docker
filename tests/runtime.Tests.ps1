74959835bbddf5e4b6915185de54829436d34b1c
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-11-17 20:56:52 +0100

    chore(Windows): remove `global:` and put test variables declarations in `BeforeDiscovery` blocks

--- tests/runtime.Tests.ps1
+++ tests/runtime.Tests.ps1
@@ -1,17 +1,19 @@
 Import-Module -DisableNameChecking -Force $PSScriptRoot/../jenkins-support.psm1
 Import-Module -DisableNameChecking -Force $PSScriptRoot/test_helpers.psm1
 
-$global:SUT_IMAGE=Get-SutImage
-$global:SUT_CONTAINER=Get-SutImage
-$global:TEST_TAG=$global:SUT_IMAGE.Replace('pester-jenkins-', '')
+BeforeDiscovery {
+  $SUT_IMAGE=Get-SutImage
+  $SUT_CONTAINER=Get-SutImage
+  $TEST_TAG=$SUT_IMAGE.Replace('pester-jenkins-', '')
+}
 
-Describe "[runtime > $global:TEST_TAG] build image" {
+Describe "[runtime > $TEST_TAG] build image" {
   BeforeEach {
     Push-Location -StackName 'jenkins' -Path "$PSScriptRoot/.."
   }
 
   It 'builds image' {
-    $exitCode, $stdout, $stderr = Build-Docker $global:SUT_IMAGE
+    $exitCode, $stdout, $stderr = Build-Docker $SUT_IMAGE
     $exitCode | Should -Be 0
   }
 
@@ -20,43 +22,43 @@ Describe "[runtime > $global:TEST_TAG] build image" {
   }
 }
 
-Describe "[runtime > $global:TEST_TAG] cleanup container" {
+Describe "[runtime > $TEST_TAG] cleanup container" {
   It 'cleanup' {
-    Cleanup $global:SUT_CONTAINER | Out-Null
+    Cleanup $SUT_CONTAINER | Out-Null
   }
 }
 
-Describe "[runtime > $global:TEST_TAG] test multiple JENKINS_OPTS" {
+Describe "[runtime > $TEST_TAG] test multiple JENKINS_OPTS" {
   It '"--help --version" should return the version, not the help' {
     # need the last line of output
-    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "run --rm -e JENKINS_OPTS=`"--help --version`" --name $global:SUT_CONTAINER -P $global:SUT_IMAGE"
+    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "run --rm -e JENKINS_OPTS=`"--help --version`" --name $SUT_CONTAINER -P $SUT_IMAGE"
     $exitCode | Should -Be 0
     $stdout -split '`n' | %{$_.Trim()} | Select-Object -Last 1 | Should -Be $env:JENKINS_VERSION
   }
 }
 
-Describe "[runtime > $global:TEST_TAG] test jenkins arguments" {
+Describe "[runtime > $TEST_TAG] test jenkins arguments" {
   It 'running --help --version should return the version, not the help' {
     # need the last line of output
-    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "run --rm --name $global:SUT_CONTAINER -P $global:SUT_IMAGE --help --version"
+    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "run --rm --name $SUT_CONTAINER -P $SUT_IMAGE --help --version"
     $exitCode | Should -Be 0
     $stdout -split '`n' | %{$_.Trim()} | Select-Object -Last 1 | Should -Be $env:JENKINS_VERSION
   }
 
   It 'version in docker metadata' {
-    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "inspect -f `"{{index .Config.Labels \`"org.opencontainers.image.version\`"}}`" $global:SUT_IMAGE"
+    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "inspect -f `"{{index .Config.Labels \`"org.opencontainers.image.version\`"}}`" $SUT_IMAGE"
     $exitCode | Should -Be 0
     $stdout.Trim() | Should -Match $env:JENKINS_VERSION
   }
 
   It 'commit SHA in docker metadata' {
-    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "inspect -f `"{{index .Config.Labels \`"org.opencontainers.image.revision\`"}}`" $global:SUT_IMAGE"
+    $exitCode, $stdout, $stderr = Run-Program 'docker.exe' "inspect -f `"{{index .Config.Labels \`"org.opencontainers.image.revision\`"}}`" $SUT_IMAGE"
     $exitCode | Should -Be 0
     $stdout.Trim() | Should -Match $env:COMMIT_SHA
   }
 }
 
-Describe "[runtime > $global:TEST_TAG] passing JVM parameters" {
+Describe "[runtime > $TEST_TAG] passing JVM parameters" {
   BeforeAll {
     $tzSetting = '-Duser.timezone=Europe/Madrid'
     $tzRegex = [regex]::Escape("Europe/Madrid")
@@ -72,32 +74,32 @@ Describe "[runtime > $global:TEST_TAG] passing JVM parameters" {
         [string] $JENKINS_JAVA_OPTS
       )
 
-      $cmd = "docker --% run -d --name $global:SUT_CONTAINER -P"
+      $cmd = "docker --% run -d --name $SUT_CONTAINER -P"
       if ($JAVA_OPTS.length -gt 0) {
         $cmd += " -e JAVA_OPTS=`"$JAVA_OPTS`""
       }
       if ($JENKINS_JAVA_OPTS.length -gt 0) {
         $cmd += " -e JENKINS_JAVA_OPTS=`"$JENKINS_JAVA_OPTS`""
       }
-      $cmd += " $global:SUT_IMAGE"
+      $cmd += " $SUT_IMAGE"
 
       Invoke-Expression $cmd
       $lastExitCode | Should -Be 0
 
       # give time to eventually fail to initialize
       Start-Sleep -Seconds 5
-      Retry-Command -RetryCount 3 -Delay 1 -ScriptBlock { docker inspect -f "{{.State.Running}}" $global:SUT_CONTAINER ; if($lastExitCode -ne 0) { throw('Docker inspect failed') } } -Verbose | Should -BeTrue
+      Retry-Command -RetryCount 3 -Delay 1 -ScriptBlock { docker inspect -f "{{.State.Running}}" $SUT_CONTAINER ; if($lastExitCode -ne 0) { throw('Docker inspect failed') } } -Verbose | Should -BeTrue
 
       # it takes a while for jenkins to be up enough
-      Retry-Command -RetryCount 30 -Delay 5 -ScriptBlock { Test-Url $global:SUT_CONTAINER "/api/json" } -Verbose | Should -BeTrue
+      Retry-Command -RetryCount 30 -Delay 5 -ScriptBlock { Test-Url $SUT_CONTAINER "/api/json" } -Verbose | Should -BeTrue
     }
 
     function Get-Csp-Value() {
-      return (Run-In-Script-Console $global:SUT_CONTAINER "System.getProperty('hudson.model.DirectoryBrowserSupport.CSP')")
+      return (Run-In-Script-Console $SUT_CONTAINER "System.getProperty('hudson.model.DirectoryBrowserSupport.CSP')")
     }
 
     function Get-Timezone-Value() {
-      return (Run-In-Script-Console $global:SUT_CONTAINER "System.getProperty('user.timezone')")
+      return (Run-In-Script-Console $SUT_CONTAINER "System.getProperty('user.timezone')")
     }
   }
 
@@ -123,6 +125,6 @@ Describe "[runtime > $global:TEST_TAG] passing JVM parameters" {
   }
 
   AfterEach {
-    Cleanup $global:SUT_CONTAINER | Out-Null
+    Cleanup $SUT_CONTAINER | Out-Null
   }
 }
