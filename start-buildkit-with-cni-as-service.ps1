6ae6422620d2193b16c08093a4def850bd4f09ac
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-02-20 20:42:45 +0100

    service after declaration

--- start-buildkit-with-cni-as-service.ps1
+++ start-buildkit-with-cni-as-service.ps1
@@ -1,11 +1,5 @@
 ## From https://github.com/moby/buildkit/blob/c8178843b73773f1703bd73f5101041da4506157/docs/windows.md#setup-instructions
 
-# # buildkitd on Windows depends on containerd. You can make the above registered buildkitd service dependent on containerd (the naming may vary). The space after = is required:
-# sc.exe config buildkitd depend= containerd
-
-# # We can also set the service to start automatically:
-# Set-Service -StartupType Automatic buildkitd
-
 buildkitd `
     --register-service `
     --service-name buildkitd `
@@ -13,3 +7,10 @@ buildkitd `
     --containerd-cni-binary-dir="C:\Program Files\containerd\cni\bin" `
     --debug `
     --log-file="C:\Windows\Temp\buildkitd.log"
+
+# buildkitd on Windows depends on containerd. You can make the above registered buildkitd service dependent on containerd (the naming may vary). The space after = is required:
+sc.exe config buildkitd depend= containerd
+
+# We can also set the service to start automatically:
+Set-Service -StartupType Automatic buildkitd
+
